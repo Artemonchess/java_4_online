0e5b75f7bdb52f766fd66fe6cf5a12248663ded8
Artem <artemonchess@gmail.com>
2022-12-08 00:55:28 +0200

    minimal_proj

---
+++ unit_1_compile/java/Level_1/minimal_proj/src/ua/com/alevel/Hoy.java
@@ -0,0 +1,11 @@
+
+package ua.com.alevel;
+
+import ua.com.alevel.test.Aloha;
+
+class Hoy {
+    public static void main(String[] args) {
+        Aloha msg = new Aloha();
+        msg.showAloha();
+    }
+}
---
+++ unit_1_compile/java/Level_1/minimal_proj/src/ua/com/alevel/test/Aloha.java
@@ -0,0 +1,8 @@
+package ua.com.alevel.test;
+
+public class Aloha {
+
+    public void showAloha() {
+        System.out.println("Bad Hello world!!!!");
+    }
+}
