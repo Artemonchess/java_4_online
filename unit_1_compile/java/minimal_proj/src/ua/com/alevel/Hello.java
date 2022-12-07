2a7b0e40dd44acf3394aebc159dbc9325970a3b1
Artem <artemonchess@gmail.com>
2022-12-07 08:17:07 +0200

    add . gcc compilation

--- unit_1_compile/java/minimal_proj/src/ua/com/alevel/Hello.java
+++ unit_1_compile/java/minimal_proj/src/ua/com/alevel/Hello.java
@@ -4,7 +4,6 @@ import ua.com.alevel.test.Message;
 
 class Hello {
     public static void main(String[] args) {
-//        ua.com.alevel.test.Message msg = new ua.com.alevel.test.Message();
         Message msg = new Message();
         msg.showMessage();
     }
