5c7311e0cecab93411d05304b21d99075348da2e
Artem <artemonchess@gmail.com>
2022-12-19 21:47:58 +0200

    A

--- unit_1_compile/java/run.jar.sh
+++ unit_1_compile/java/run.jar.sh
@@ -4,36 +4,36 @@ echo 'run level 1'
 
 echo 'run simple'
 cd ./level_1/simple
-javac Series.java
-java Series
+javac Hoy.java
+java Hoy
 
 cd ../
 
 echo 'run package'
 cd ./package
-javac ua/com/alevel/Series.java
-java ua.com.alevel.Series
+javac ua/com/alevel/Hoy.java
+java ua.com.alevel.Hoy
 
 cd ../
 
 echo 'run separate packages'
 cd ./separate_packages
-javac ua/com/alevel/Series.java
-java ua.com.alevel.Series
+javac ua/com/alevel/Hoy.java
+java ua.com.alevel.Hoy
 
 cd ../
 
 echo 'run minimal proj'
 cd ./minimal_proj
-javac -sourcepath ./src -d build/classes ./src/ua/com/alevel/Series.java
-java -cp build/classes ua.com.alevel.Series
+javac -sourcepath ./src -d build/classes ./src/ua/com/alevel/Hoy.java
+java -cp build/classes ua.com.alevel.Hoy
 
 cd ../
 
 echo 'run med proj a'
 cd ./med_proj
-javac -sourcepath ./src -d build/classes ./src/ua/com/alevel/Series.java
-jar cvfm build/jar/series.jar resources/MANIFEST.MF -C build/classes .
-java -jar build/jar/series.jar
+javac -sourcepath ./src -d build/classes ./src/ua/com/alevel/Hoy.java
+jar cvfm build/jar/Hoy.jar resources/MANIFEST.MF -C build/classes .
+java -jar build/jar/Hoy.jar
 
 cd ../../
