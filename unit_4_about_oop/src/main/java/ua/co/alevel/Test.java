1919308ef4bffdc2b9aa83fe9926a53c3227b7d4
Artem <artemonchess@gmail.com>
2022-12-19 11:11:08 +0200

    A

---
+++ unit_4_about_oop/src/main/java/ua/co/alevel/Test.java
@@ -0,0 +1,12 @@
+package ua.co.alevel;
+
+public class Test {
+
+        String name = "Aloha";
+        boolean isStupid = false;
+
+    public void sendMessage(String message) {
+        System.out.println(message);
+    }
+}
+
